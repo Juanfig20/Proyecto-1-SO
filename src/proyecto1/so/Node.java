fe15a4d96cd717e3c601757e7935933b819af2f0
Juanfig20 <juan.figuera@correo.unimet.edu.ve>
2024-09-29 19:44:42 -0400

    Listas

--- src/proyecto1/so/Node.java
+++ src/proyecto1/so/Node.java
@@ -8,6 +8,34 @@ package proyecto1.so;
  *
  * @author User
  */
-public class Node {
+public class Node<T> {
+    private T element;
+    private Node<T> pnext;
+
+    public Node(T element, Node<T> pnext) {
+        this.element = element;
+        this.pnext = pnext;
+    }
+
+    public Node(T element) {
+        this.element = element;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public Node<T> getPnext() {
+        return pnext;
+    }
+
+    public void setPnext(Node<T> pnext) {
+        this.pnext = pnext;
+    }
+    
     
 }
