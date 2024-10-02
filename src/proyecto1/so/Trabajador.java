36a24701d62ae2c71e76815f1f66a129eb2b05a8
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2024-10-01 23:28:38 -0400

    Añadida clase Trabajador

---
+++ src/proyecto1/so/Trabajador.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1.so;
+
+/**
+ *
+ * @author manza
+ */
+public class Trabajador {
+    private int tipo;
+    private int salario;
+    private float salarioAcumulado;
+    private int cantidadTrabajadores;
+    private int duracionDia;
+    private int diasRestantes;
+    private int contadorDias;
+    private Almacen almacen;
+    //private Semaforo semaforo;
+    
+    
+}
+
